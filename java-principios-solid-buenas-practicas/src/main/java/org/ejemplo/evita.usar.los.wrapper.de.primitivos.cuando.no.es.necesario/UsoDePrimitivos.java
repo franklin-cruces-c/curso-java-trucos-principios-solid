cd6777012ca74146e33270ecd8550024f99d0ec0
franklin-cruces <franklin.cruces@hotmail.com>
2025-09-07 02:02:02 +0200

    evita usar los wrapper de primitivos cuando no es necesario - Si trabajamos con colecciones muy grandes es mejor utilizar primitivos en lugar de los wrapper  ya que requieren más memoria y se demoran más tiempo  en el procesamiento

---
+++ java-principios-solid-buenas-practicas/src/main/java/org/ejemplo/evita.usar.los.wrapper.de.primitivos.cuando.no.es.necesario/UsoDePrimitivos.java
@@ -0,0 +1,19 @@
+package org.ejemplo.evita.usar.los.wrapper.de.primitivos.cuando.no.es.necesario;
+
+public class UsoDePrimitivos {
+
+    private static long[] numeros = new long[10_000_000];
+
+    public static void main(String[] args) throws InterruptedException {
+        long antes = System.currentTimeMillis();
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (long) i;
+        }
+
+        System.out.println("Finalizado");
+        Thread.sleep(30000);
+        long ahora = System.currentTimeMillis();
+        System.out.println("Tiempo transcurrido: " + (ahora - antes));
+
+    }
+}
--- java-principios-solid-buenas-practicas/src/main/java/org/ejemplo/evita.usar.los.wrapper.de.primitivos.cuando.no.es.necesario/UsoDeWrappers.java
+++ java-principios-solid-buenas-practicas/src/main/java/org/ejemplo/evita.usar.los.wrapper.de.primitivos.cuando.no.es.necesario/UsoDeWrappers.java
@@ -1,15 +1,21 @@
 package org.ejemplo.evita.usar.los.wrapper.de.primitivos.cuando.no.es.necesario;
 
 public class UsoDeWrappers {
-
-    private static long[] numeros = new long[10_000_000];
+    /*
+     * Si trabajamos con colecciones muy grandes es mejor
+     * utilizar primitivos en lugar de los wrapper
+     * ya que requieren más memoria y se demoran más tiempo
+     * en el procesamiento
+     */
+    private static Long[] numeros = new Long[10_000_000];
 
     public static void main(String[] args) throws InterruptedException {
-
+        long antes = System.currentTimeMillis();
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = (long) i;
         }
-        
+        long ahora = System.currentTimeMillis();
+        System.out.println("Tiempo transcurrido: "+ (ahora-antes));
         System.out.println("Finalizado");
         Thread.sleep(30000);
 
